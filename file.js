2435328652f7487c7e2b35ae1bdde0e95912f1b7
KarimHazem186 <karimhazem186@gmail.com>
2025-04-06 00:33:44 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,16 @@
+// Import Export
+/* Export تعمل توزيع للحاجات انت عملتها في مكان اخر  
+بتخليني اقدر استخدمه في مكان تاني
+*/
+/*export var x = 5;
+
+export var y =6;*/
+/////////////////////////////////////////
+/* export defult test ; => import fsrew from '' */
+/*function test() {
+    console.log("test");
+}
+ export default test; // export default معناها حاجة واحده بعملها مره واحده بس
+ */
+
+
---
+++ main.js
@@ -0,0 +1,19 @@
+ /* import  عشان تجيب الحاجة عاوز تجيب اي تجيبها منين 
+ export var x = 5 => import {x} from ''
+ */
+/*import  { x , y } from "./file.js" // ./ معناها اتنين في نفس المسار
+
+console.log(x*y);
+*/
+
+// export default   بنادي عليها بأي اسم او نفس الاسم عادي
+/*
+import ewewrr from "./file.js";
+ewewrr();
+*/
+/////////////////////////////////////////////////////////
+
+// https://opentdb.com/api.php?amount=10&category=9&difficulty=easy
+
+import Settings from "./Quiz/settings.js";
+new Settings();
